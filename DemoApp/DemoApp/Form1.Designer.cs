bc52b279f8990b0da2048da10f2cbf80295999a7
keyurpts <keyur.shimpi@prototechsolutions.com>
2024-04-11 14:15:12 +0530

    Added changes to test clickonce functionality using azure blob.

--- DemoApp/DemoApp/Form1.Designer.cs
+++ DemoApp/DemoApp/Form1.Designer.cs
@@ -35,11 +35,15 @@ namespace DemoApp
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(127, 75);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -48,7 +52,7 @@ namespace DemoApp
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(307, 95);
+            this.button2.Location = new System.Drawing.Point(12, 51);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -58,7 +62,7 @@ namespace DemoApp
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(444, 188);
+            this.button3.Location = new System.Drawing.Point(12, 80);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(138, 26);
             this.button3.TabIndex = 2;
@@ -67,7 +71,7 @@ namespace DemoApp
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(191, 200);
+            this.button4.Location = new System.Drawing.Point(12, 118);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 3;
@@ -76,7 +80,7 @@ namespace DemoApp
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(553, 118);
+            this.button5.Location = new System.Drawing.Point(12, 147);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 4;
@@ -85,18 +89,58 @@ namespace DemoApp
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(665, 118);
+            this.button6.Location = new System.Drawing.Point(12, 176);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(75, 23);
             this.button6.TabIndex = 5;
             this.button6.Text = "button6";
             this.button6.UseVisualStyleBackColor = true;
             // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 205);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(12, 352);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(12, 381);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(12, 410);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
@@ -117,6 +161,10 @@ namespace DemoApp
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
     }
 }
 
